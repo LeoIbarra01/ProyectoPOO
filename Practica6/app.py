3bd44535d30dc945766006f21e7c752e4b9d1fc2
LeoIbarra01 <122042401@upq.edu.mx>
2024-06-14 19:27:13 -0600

    Add files via upload

--- Practica6/app.py
+++ Practica6/app.py
@@ -1,23 +1,25 @@
-from flask import Flask, request,render_template,url_for,redirect,flash
+from flask import Flask, request, render_template, url_for, redirect, flash
 from flask_mysqldb import MySQL
 
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = "localhost"
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'medicos'
+mysql = MySQL(app)
 
-app= Flask(__name__)
-app.config['MYSQL_HOST']="localhost"
-app.config['MYSQL_USER']='root'
-app.config['MYSQL_PASSWORD']=''
-app.config['MYSQL_DB']='tbmedicos'
-mysql= MySQL(app)
-
-app.secret_key='mysecretkey'
+app.secret_key = 'mysecretkey'
 
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/formulario')
+def formulario():
+    return render_template('formulario.html')
 
-@app.route('/registroMedico', methods=['GET'])
-def registroMedico():
-    if request.method == 'GET':
+@app.route('/registrarMedico', methods=['POST'])
+def registrarMedico():
+    if request.method == 'POST':
         # Tomamos los datos que vienen por POST
         Frfc = request.form['txtRfc']
         Fnombre = request.form['txtNombre']
@@ -25,21 +27,18 @@ def registroMedico():
         Fcorreo = request.form['txtCorreo']
         Fpassword = request.form['txtPassword']
         Frol = request.form['txtRol']
-
         
         # Enviamos a la BD sin incluir la columna de clave primaria
         cursor = mysql.connection.cursor()
-        cursor.execute('INSERT INTO tbmedicos(RFC, Nom_Completo, Cedula, correro, password, ROL) VALUES (%s, %s, %s,%s, %s, %s)', (Frfc, Fnombre, Fcedula, Fcorreo, Fpassword, Frol))
+        cursor.execute('INSERT INTO tbmedicos1(RFC, Nombre, Cedula, Correo, Password, Rol) VALUES (%s, %s, %s, %s, %s, %s)', (Frfc, Fnombre, Fcedula, Fcorreo, Fpassword, Frol))
         mysql.connection.commit()
         flash('Registro Guardado Correctamente')
         return redirect(url_for('formulario'))
 
-
-
-#Manejador de exepciones 
+# Manejador de excepciones
 @app.errorhandler(404)
 def paginano(e):
-    return'Revisa tu sintaxis, no encontre nada'
+    return 'Revisa tu sintaxis, no encontré nada'
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(port=3000, debug=True)
